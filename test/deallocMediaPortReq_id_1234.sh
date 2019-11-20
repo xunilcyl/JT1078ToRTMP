cf6dcc5a9efe4ec3cbb393e83611763dfc2bb9ad
test <test@192.168.0.107>
2019-11-20 12:02:46 +0800

    add test file

---
+++ test/deallocMediaPortReq_id_1234.sh
@@ -0,0 +1,1 @@
+curl -v -d "{\"method\":\"deallocMediaPortReq\", \"params\":{\"uniqueID\": "1234"}, \"id\":100}" "127.0.0.1:8888"
---
+++ test/publishTest.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+function publish() {
+    allocResult=`curl -v -d "{\"method\":\"allocMediaPortReq\", \"params\":{\"uniqueID\": "$1"}, \"id\":100}" "127.0.0.1:8888" | grep \"port\" | awk '{print $2}'`
+    if [ -z $allocResult ]; then
+        echo "allocate port failed"
+        return 1
+    fi
+
+    echo $allocResult
+    port=${allocResult:1:5}
+    if [ -z $port ]; then
+        echo "port invalid: $port"
+    fi
+    
+    echo "get port $port"
+    
+    ./h264sender $port mediaStream1078_fn_fps_24s.bin 24 1 &
+    
+    if [ $? -eq 0 ]; then
+        echo "send media stream success."
+    else
+        echo "send media failed"
+        return 2;
+    fi
+}
+
+if [ $# -lt 1 ]; then
+    echo "usage: $0 <num>"
+    echo "       <num> represents the number of publishers"
+    exit 1
+fi
+
+for ((i=1; i<=$1; i++))
+do
+    publish $i
+done
+
+exit 0;
+
